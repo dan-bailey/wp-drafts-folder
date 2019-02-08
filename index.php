1008f0e55575adfdac5e4f309e4d656e20ce7fa4
Dan Bailey <dbailey@danbailey.net>
2019-02-08 06:50:06 -0600

    housekeeping

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 function add_drafts_admin_menu_item() {
-  // $page_title, $menu_title, $capability, $menu_slug, $callback_function
+  // adds "Drafts" to the Posts menu in the Admin view
   add_posts_page(__('Drafts'), __('Drafts'), 'read', 'edit.php?post_status=draft&post_type=post');
 }
 add_action('admin_menu', 'add_drafts_admin_menu_item');
